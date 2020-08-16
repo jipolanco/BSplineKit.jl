0a71f253c4bf9de048932ca8d90430279ea1a10d
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-08-16 15:45:07 +0200

    Docs: update matomo.js

--- docs/src/assets/matomo.js
+++ docs/src/assets/matomo.js
@@ -1,6 +1,5 @@
-var _paq = window._paq || [];
+var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
@@ -8,5 +7,5 @@ _paq.push(['enableLinkTracking']);
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '3']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
