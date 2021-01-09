1dcc907f6d4a44a7fe6c07f81ade06fbc3870eae
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-01-09 13:12:22 +0100

    Update docs assets

--- docs/src/assets/matomo.js
+++ docs/src/assets/matomo.js
@@ -3,9 +3,9 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-    var u="https://bsadioqrywqelklksjd.tk/matomo/";
+    var u="https://goeiwkensasd.tk/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '3']);
+    _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
