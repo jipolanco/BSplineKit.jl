b4a999d22a159b682030ba6b373696628ca4c8ce
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-07-09 16:57:55 +0200

    Some updates to example

--- examples/heat.jl
+++ examples/heat.jl
@@ -10,28 +10,19 @@
 # in a bounded domain ``x ∈ [-1, 1]`` with homogeneous Neumann boundary
 # conditions, ``∂_x θ(±1, t) = 0``.
 
-# ## Initial condition
-
-# We impose the initial condition
-#
-# ```math
-# θ(x, 0) = θ_0(x) = 1 + \cos(π x).
-# ```
-
-using CairoMakie
-
-θ₀(x) = 1 + cos(π * x)
-
-fig = Figure()
-ax = Axis(fig[1, 1]; xlabel = "x", ylabel = "θ")
-plot!(ax, -1..1, θ₀; label = "θ₀(x)")
-fig
-
 # ## Defining a B-spline basis
 
 # The general idea is to approximate the unknown solution by a spline of order
 # ``k``.
-# For this, we first need to define a B-spline basis.
+# For this, we first define a B-spline basis ``\{ b_i(x); \, i = 1, …, N \}``, such
+# that the solution at a given time $t$ is approximated by
+#
+# ```math
+# θ(x, t) = ∑_{i = 1}^N v_i(t) b_i(x),
+# ```
+#
+# where the $v_i$ are the B-spline coefficients.
+#
 # A B-spline basis is uniquely defined by its order ``k`` and by a choice of
 # *knot* locations within the spatial domain, which form the spatial grid.
 
@@ -43,7 +34,10 @@ using BSplineKit
 B = BSplineBasis(BSplineOrder(4), knots_in)
 
 # Note that the generated basis includes an *augmented* set of knots, in which
-# each boundary is repeated ``k`` times.
+# each boundary is repeated ``k`` times:
+
+knots(B)
+
 # In other words, the boundary knots have multiplicity ``k``, while interior
 # knots have multiplicity 1.
 # This is common practice in bounded domains, and translates the fact that the
@@ -53,17 +47,31 @@ B = BSplineBasis(BSplineOrder(4), knots_in)
 # This behaviour can be disabled via the `augment` argument of
 # [`BSplineBasis`](@ref).
 
-# We can now plot the knot locations and the generated B-spline basis:
+# We can now plot the knot locations (crosses) and the generated B-spline basis:
 
-function plot_basis!(ax, B; eval_args = ())
-    ts = knots(B)
-    plot!(ax, ts, zero(ts); marker = :x, color = :red)
+using CairoMakie
+CairoMakie.activate!(type = "svg")
+
+function plot_knots!(ax, ts; knot_offset = 0.03)
+    ys = zero(ts)
+    ## Add offset to distinguish knots with multiplicity > 1
+    for i in eachindex(ts)[(begin + 1):end]
+        if ts[i] == ts[i - 1]
+            ys[i] = ys[i - 1] + knot_offset
+        end
+    end
+    scatter!(ax, ts, ys; marker = :x, markersize = 20, color = :gray)
+    ax
+end
+
+function plot_basis!(ax, B; eval_args = (), kws...)
     cmap = cgrad(:tab20)
     N = length(B)
     for (n, bi) in enumerate(B)
         color = cmap[(n - 1) / (N - 1)]
         lines!(ax, -1..1, x -> bi(x, eval_args...); color, linewidth = 2.5)
     end
+    plot_knots!(ax, knots(B); kws...)
     ax
 end
 
@@ -101,7 +109,7 @@ fig
 
 fig = Figure()
 ax = Axis(fig[1, 1]; xlabel = "x", ylabel = "ϕᵢ′(x)")
-plot_basis!(ax, R; eval_args = (Derivative(1), ))
+plot_basis!(ax, R; eval_args = (Derivative(1), ), knot_offset = 0.4)
 fig
 
 # Note that the new basis has two less functions than the original one,
@@ -150,6 +158,45 @@ T = recombination_matrix(R)
 # ``f(x) = ∑_j u_j \, ϕ_j(x)``, its coefficients ``v_i`` in the original basis
 # can be obtained via the matrix-vector multiplication ``\bm{v} = \mathbf{T} \bm{u}``.
 
+# ## Initial condition
+
+# We come back now to the heat equation.
+# We want to impose the following initial condition:
+#
+# ```math
+# θ(x, 0) = θ_0(x) = 1 + \cos(π x).
+# ```
+
+# First, we want to approximate this initial condition in the recombined
+# B-spline basis that we have just constructed.
+# This may be easily done using [`approximate`](@ref), which interpolates the
+# function by evaluating it over a discrete set of interpolation points.
+
+θ₀(x) = 1 + cos(π * x)
+θ₀_spline = approximate(θ₀, R)
+
+# Note that the interpolation points don't include the boundaries, since there
+# the boundary conditions are exactly satisfied by the spline approximation.
+
+# To see that everything went well, we can plot the exact initial condition and
+# its spline approximation, which show no important differences.
+
+fig = Figure(resolution = (800, 400))
+let ax = Axis(fig[1, 1]; xlabel = "x", ylabel = "θ")
+    lines!(ax, -1..1, θ₀; label = "θ₀(x)", color = :blue)
+    lines!(ax, -1..1, x -> θ₀_spline(x); label = "Approximation", color = :orange, linestyle = :dash)
+    axislegend(ax; position = :cb)
+end
+let ax = Axis(fig[1, 2]; xlabel = "x", ylabel = "Difference")
+    lines!(ax, -1..1, x -> θ₀(x) - θ₀_spline(x))
+end
+fig
+
+# Note that we have access to the B-spline coefficients ``v_i`` associated to
+# the initial condition, which we will use further below:
+
+v_init = coefficients(θ₀_spline)
+
 # ## Expanding the solution
 #
 # To solve the governing equation, the strategy is to project the unknown
@@ -157,7 +204,7 @@ T = recombination_matrix(R)
 # That is, we approximate the solution as
 #
 # ```math
-# θ(x, t) = \sum_{j = 1}^M θ_j(t) \, ϕ_j(x).
+# θ(x, t) = \sum_{j = 1}^M u_j(t) \, ϕ_j(x).
 # ```
 #
 # Plugging this representation into the heat equation, we find
@@ -168,3 +215,26 @@ T = recombination_matrix(R)
 # ```
 #
 # where primes denote spatial derivatives.
+#
+# We thus have ``M`` unknowns $θ_j$.
+# We can use the [method of mean weighted
+# residuals](https://en.wikipedia.org/wiki/Method_of_mean_weighted_residuals) to
+# find the coefficients $θ_j$, by projecting the above equation onto a chosen
+# set of *test* functions $φ_i$:
+#
+# ```math
+# ∑_j \frac{\mathrm{d} θ_j}{\mathrm{d} t} \, ⟨φ_i, ϕ_j⟩ = ∑_j θ_j \, ⟨φ_i, ϕ_j''⟩,
+# ```
+#
+# where ``⟨ f, g ⟩ = ∫_0^1 f(x) \, g(x) \, \mathrm{d} x`` is the inner product
+# between functions.
+#
+# Two of the most common choices of test functions are:
+#
+# - ``φ_i(x) = δ(x - x_i)``, where ``δ`` is Dirac's delta, and ``x_i`` are a set of
+#   *collocation* points where the equation will be satisfied.
+#   This approach is known as the **collocation** method.
+#
+# - ``φ_i(x) = ϕ_i(x)``, in which case this is the **Galerkin** method.
+#
+# We describe the solution using both methods in the following.
