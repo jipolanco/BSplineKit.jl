7bc3ebd84440b4f6d2da0ae0e73082de99329d69
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-07-30 22:46:26 +0200

    Add some approximation tests

--- test/approximation.jl
+++ test/approximation.jl
@@ -1,34 +1,52 @@
 using BSplineKit
 using Test
 
+# TODO
+# - test recombined basis (test function should satisfy BCs...)
+
+function check_approximate_in_place!(f, fapprox)
+    S = copy(spline(fapprox))  # assumed to already approximate `f`
+    @test 32 ≥ @allocated approximate!(f, fapprox)  # (almost) no allocations!
+    @test spline(fapprox) == S  # check that nothing changed
+    nothing
+end
+
 function test_approximation(B)
     k = order(B)
 
-    fpoly = let poly_coefs = ntuple(d -> (-d)^d, Val(k))
-        x -> @eval_poly(x, poly_coefs...)
-    end
+    ftest = sin
 
-    f_fast = approximate(fpoly, B, VariationDiminishing())
-
-    # @test repr(f_fast)
+    f_fast = @inferred approximate(ftest, B, VariationDiminishing())
+    @test startswith(repr(f_fast), "SplineApproximation containing")
+    check_approximate_in_place!(ftest, f_fast)
 
     # With these two methods, the approximation should exactly match a
     # polynomial of degree k - 1.
-    f_interp = approximate(fpoly, B, ApproxByInterpolation(B))
+    f_interp = @inferred approximate(ftest, B, ApproxByInterpolation(B))
+    @test startswith(repr(f_interp), "SplineApproximation containing")
+    check_approximate_in_place!(ftest, f_interp)
 
-    # TODO test this method...
-    # f_opt = approximate(fpoly, B, MinimiseL2Error(B))
+    f_opt = @inferred approximate(ftest, B, MinimiseL2Error())
+    @test startswith(repr(f_opt), "SplineApproximation containing")
+    check_approximate_in_place!(ftest, f_opt)
 
-    # xfine = range(first(x), last(x); length = 2 * length(x))
-    # @test fapprox.(xfine) ≈ fpoly.(xfine)
+    # Polynomial of degree k - 1.
+    # The interpolation and minimisation methods should exactly approximate such
+    # a polynomial.
+    poly_coefs = ntuple(d -> (-d)^d, Val(k))
+    fpoly(x) = evalpoly(x, poly_coefs)
+
+    xfine = range(boundaries(B)...; length = 3 * length(B))
+
+    approximate!(fpoly, f_interp)
+    @test fpoly.(xfine) ≈ f_interp.(xfine)
+
+    approximate!(fpoly, f_opt)
+    @test fpoly.(xfine) ≈ f_opt.(xfine)
+
+    nothing
 end
 
-# TODO
-# - test a couple of orders
-# - test the 3 approximation methods
-# - test in-place approximation
-# - test show(::SplineApproximation)
-# - test recombined basis (test function should satisfy BCs...)
 @testset "Approximation" begin
     N = 16
     breaks = [-cos(π * n / N) for n = 0:N]
