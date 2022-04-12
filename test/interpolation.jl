53a1dee694ecead682ca4e51e3bb39d7d6c9efa4
Juan Ignacio Polanco <jipolanc@gmail.com>
2022-04-12 10:42:52 +0200

    Update tests

--- test/interpolation.jl
+++ test/interpolation.jl
@@ -1,10 +1,21 @@
 using Random
+using StaticArrays: SVector
 
-function test_interpolation(ord::BSplineOrder)
+function test_interpolation(ord::BSplineOrder, ::Type{Ty} = Float64) where {Ty}
     rng = MersenneTwister(42)
     ndata = 40
     xs = sort(randn(rng, ndata))
-    ys = randn(rng, ndata)
+
+    # This is Int   if Ty = Int
+    #         Int32 if Ty = SVector{N, Int32}
+    Tdata = eltype(Ty)
+
+    ys = if Tdata <: Integer
+        [rand(rng, Ty) .% Ty(16) for _ = 1:ndata]  # values in -16:16
+    else
+        randn(rng, Ty, ndata)
+    end
+
     k = order(ord)
 
     @testset "No BCs" begin
@@ -30,7 +41,7 @@ function test_interpolation(ord::BSplineOrder)
             for n = 2:(k ÷ 2)
                 Sder = Derivative(n) * S
                 for x ∈ boundaries(basis(S))
-                    @test abs(Sder(x)) < abs(S(x)) * 1e-7
+                    @test norm(Sder(x)) < norm(S(x)) * 1e-7
                 end
             end
         end
@@ -41,6 +52,10 @@ end
 
 @testset "Interpolation" begin
     @testset "k = $k" for k ∈ (3, 4, 6, 8)
-        test_interpolation(BSplineOrder(k))
+        test_interpolation(BSplineOrder(k), Float64)
+    end
+    types = (Int32, ComplexF32, SVector{2, Float32})
+    @testset "T = $T" for T ∈ types
+        test_interpolation(BSplineOrder(4), T)
     end
 end
