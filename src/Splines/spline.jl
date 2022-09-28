2b2568662b11a64e5795ba55aeb9efbe1f91f478
Juan Ignacio Polanco <jipolanc@gmail.com>
2022-09-28 11:20:50 +0200

    More docs

--- src/Splines/spline.jl
+++ src/Splines/spline.jl
@@ -313,6 +313,21 @@ end
 Returns an antiderivative of the given spline as a new spline.
 
 The algorithm is described in de Boor 2001, p. 127.
+
+Note that the integral spline `I` returned by this function is defined up to a
+constant.
+By convention, here the returned spline `I` is zero at the left boundary of the
+domain.
+One usually cares about the integral of `S` from point `a` to point `b`, which
+can be obtained as `I(b) - I(a)`.
+
+!!! note "Periodic splines"
+
+    Note that the integral of a periodic function is in general not periodic.
+    For periodic splines (backed by a [`PeriodicBSplineBasis`](@ref)), this
+    function returns a non-periodic spline (backed by a regular
+    [`BSplineBasis`](@ref)).
+
 """
 function integral(S::Spline)
     B = basis(S)
