fdd98f4d50b4cd931ec8ff63c1b91ed62673b1e7
Juan Ignacio Polanco <jipolanc@gmail.com>
2023-02-17 09:13:28 +0000

    build based on 13a5c40

--- versions.js
+++ versions.js
@@ -1,5 +1,13 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.14",
+  "v0.13",
+  "v0.12",
+  "v0.11",
+  "v0.10",
+  "v0.9",
+  "v0.8",
+  "v0.7",
   "v0.6",
   "v0.5",
   "v0.4",
@@ -8,3 +16,5 @@ var DOC_VERSIONS = [
   "v0.1",
   "dev",
 ];
+var DOCUMENTER_NEWEST = "v0.14.3";
+var DOCUMENTER_STABLE = "stable";
