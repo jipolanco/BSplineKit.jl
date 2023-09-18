8357a64fb2cbf4b28954b187c74a53faed45fa60
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-09-18 12:02:09 +0200

    Use custom LazyVcatVector instead of LazyArrays (#77)
    * Use custom LazyVcatVector instead of LazyArrays
    
    This fixes allocation tests. It seems like some recent version of
    LazyArrays results in extra allocations when constructing a lazy array
    as in `ApplyArray(vcat, x, y, z)`. It's not the first time this happens,
    so we define a minimal custom type instead...
    
    * Minor changes to runtests.jl
    
    * Remove LazyArrays dependency

--- src/Recombinations/splines.jl
+++ src/Recombinations/splines.jl
@@ -1,4 +1,24 @@
-using LazyArrays: ApplyArray
+# This is similar to LazyArrays.ApplyArray(vcat, ...) but without the extra allocations.
+struct LazyVcatVector{T, Data <: Tuple{Vararg{AbstractVector}}} <: AbstractVector{T}
+    data   :: Data
+    length :: Int
+    function LazyVcatVector(data::Tuple)
+        T = Base.promote_eltype(data...)
+        foreach(Base.require_one_based_indexing, data)
+        n = sum(length, data)
+        new{T, typeof(data)}(data, n)
+    end
+end
+
+LazyVcatVector(args...) = LazyVcatVector(args)
+
+Base.size(v::LazyVcatVector) = (v.length,)
+
+Base.@propagate_inbounds Base.getindex(v::LazyVcatVector, i::Integer) = _getindex(v, i, i, v.data...)
+Base.@propagate_inbounds _getindex(v, i, j, u, etc...) = j ≤ length(u) ? u[j] : _getindex(v, i, j - length(u), etc...)
+@inline _getindex(v, i, j) = throw(BoundsError(v, i))
+
+## ================================================================================ ##
 
 """
     parent_coefficients(R::RecombinedBSplineBasis, coefs::AbstractVector)
@@ -7,7 +27,7 @@ Returns the coefficients associated to the parent B-spline basis, from the
 coefficients `coefs` in the recombined basis.
 
 Note that this function doesn't allocate, since it returns a lazy concatenation
-(via LazyArrays.jl) of two StaticArrays and a view of the `coefs` vector.
+of two StaticArrays and a view of the `coefs` vector.
 """
 function parent_coefficients(R::RecombinedBSplineBasis, coefs::AbstractVector)
     length(coefs) == length(R) ||
@@ -32,7 +52,7 @@ function parent_coefficients(R::RecombinedBSplineBasis, coefs::AbstractVector)
     yB = @inbounds view(coefs, (Na + 1):H)
     yC = C * xC
 
-    ApplyArray(vcat, yA, yB, yC)
+    LazyVcatVector(yA, yB, yC)
 end
 
 # Returns spline written in the parent basis (usually a regular BSplineBasis).
