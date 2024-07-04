aa9b33ea9796967ed1d2811f051b405edadf9623
Jon Fintzi <fintzijr@nih.gov>
2024-07-04 10:14:07 -0400

    finite diffs -> AD forward diff

--- src/BSplineKit.jl
+++ src/BSplineKit.jl
@@ -3,6 +3,7 @@ module BSplineKit
 using Reexport
 using PrecompileTools
 using LinearAlgebra: LinearAlgebra  # needed for docs
+using ForwardDiff
 
 include("BandedTensors/BandedTensors.jl")
 @reexport using .BandedTensors
