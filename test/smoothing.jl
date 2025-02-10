2fad512bbcb7289615dc3109f086d6835a4e8a6a
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-02-10 10:40:14 +0100

    Add smoothing tests using AD

--- test/smoothing.jl
+++ test/smoothing.jl
@@ -2,7 +2,66 @@
 
 using BSplineKit
 using QuadGK: quadgk
+using ReverseDiff
 using Test
+
+# This is the objective function that `fit` is supposed to minimise.
+# We can verify this using automatic differentiation: the gradient wrt the spline
+# coefficients should be zero.
+function smoothing_objective(cs, R::AbstractBSplineBasis, xs, ys; weights = nothing, λ)
+    # Construct spline from coefficients and knots
+    S = Spline(R, cs)
+    S″ = Derivative(2) * S
+
+    # Compute first term of objective (loss) function
+    T = eltype(cs)
+    loss = zero(T)
+    for i in eachindex(xs, ys)
+        w = weights === nothing ? 1 : weights[i]
+        loss += w * abs2(ys[i] - S(xs[i]))
+    end
+
+    # Integrate roughness term interval by interval
+    for i in eachindex(xs)[1:end-1]
+        # Note: S″(x) is linear within each interval, and thus the integrand is quadratic.
+        # Therefore, a two-point GL quadrature is exact (weights = 1 and locations = ±1/√3).
+        a = xs[i]
+        b = xs[i + 1]
+        Δ = (b - a) / 2
+        xc = (a + b) / 2
+        gl_weight = 1
+        gl_ξ = 1 / sqrt(T(3))
+        for ξ in (-gl_ξ, +gl_ξ)
+            x = Δ * ξ + xc
+            loss += λ * Δ * gl_weight * abs2(S″(x))
+        end
+    end
+
+    loss
+end
+
+function check_zero_gradient(S::Spline, xs, ys; weights = nothing, λ)
+    cs = coefficients(S)
+    R = basis(S)  # usually a RecombinedBSplineBasis
+
+    # Not sure how useful this is...
+    ∇f = similar(cs)  # gradient wrt coefficients
+    inputs = (cs,)
+    results = (∇f,)
+    # all_results = map(DiffResults.GradientResult, results)
+    cfg = ReverseDiff.GradientConfig(inputs)
+
+    # Compute gradient
+    ReverseDiff.gradient!(results, cs -> smoothing_objective(cs, R, xs, ys; weights, λ), inputs, cfg)
+
+    # Verify that |∇f|² is negligible. Note that is has the same units as |y_i|² ≡ Y², since
+    # f ~ Y² and therefore ∂f/∂cⱼ ~ Y. So we compare it with the sum of |y_i|².
+    reference = sum(abs2, ys)
+    err = sum(abs2, ∇f)
+    @test err / reference < 1e-12
+
+    nothing
+end
 
 # Returns the integral of |S''(x)| (the "curvature") over the whole spline.
 function total_curvature(S::Spline)
@@ -41,10 +100,11 @@ end
         λs = [0.0, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
         curvatures = similar(λs)
         distances = similar(λs)
-        for i in eachindex(λs)
-            S = @inferred fit(xs, ys, λs[i])
+        for (i, λ) in pairs(λs)
+            S = @inferred fit(xs, ys, λ)
             curvatures[i] = total_curvature(S)
             distances[i] = distance_from_data(S, xs, ys)
+            check_zero_gradient(S, xs, ys; λ)
         end
         @test issorted(curvatures; rev = true)  # in decreasing order (small λ => large curvature)
         @test issorted(distances)  # in increasing order (large λ => large distance from data)
@@ -55,10 +115,12 @@ end
         weights = fill!(similar(xs), 1)
         S = fit(xs, ys, λ)
         Sw = @inferred fit(xs, ys, λ; weights)  # equivalent to the default (all weights are 1)
+        check_zero_gradient(Sw, xs, ys; λ, weights)
         @test coefficients(S) == coefficients(Sw)
         # Now give more weight to point i = 3
         weights[3] = 1000
         Sw = fit(xs, ys, λ; weights)
+        check_zero_gradient(Sw, xs, ys; λ, weights)
         @test abs(Sw(xs[3]) - ys[3]) < abs(S(xs[3]) - ys[3])  # the new curve is closer to the data point i = 3
         @test total_curvature(Sw) > total_curvature(S)  # since we give more importance to data fitting (basically, the sum of weights is larger)
         @test distance_from_data(Sw, xs, ys) < distance_from_data(S, xs, ys)
